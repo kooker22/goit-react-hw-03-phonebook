e4d57f69587f9a38a472d5613fe54b1d5f329456
Иван Попович <kooker22>
2020-06-28 17:34:29 +0300

    Updates

---
+++ precache-manifest.e3bf1acf5a6f88328bfec09d082047a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0739d4ad64bb00c333df4a7f12a11b",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "487a9995293f00352067",
+    "url": "/goit-react-hw-03-phonebook/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "4853a50124fb65c3d88b",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.ea6939cb.chunk.css"
+  },
+  {
+    "revision": "487a9995293f00352067",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.908a7f52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.908a7f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4853a50124fb65c3d88b",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.02bd73bd.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
